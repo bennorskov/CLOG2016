1866651ea5c3e44def0d335ff8f61eb7b8c11254
Ben Norskov <ben@playistheantidote.com>
2016-09-01 18:00:44 -0400

    InClass Files from Thursday. Triggers and lights and audio

---
+++ Week01_Thursday/Assets/lightTrigger.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lightTrigger : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// when something enters the Trigger attached to this gameObject
+	void OnTriggerEnter() {
+		// get the light attached to this game object
+		// turn that light on
+		GetComponent<Light>().color = Color.cyan;
+
+	}
+	// turn the light off when you leave
+	void OnTriggerExit() {
+		GetComponent<Light>().color = Color.magenta;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ Week01_Thursday/Assets/playAudio.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playAudio : MonoBehaviour {
+
+	void OnTriggerEnter() {
+		GetComponent<AudioSource>().Play();
+	}
+}
