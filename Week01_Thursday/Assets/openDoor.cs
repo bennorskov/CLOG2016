280a01af7241c718117da00c1e205fcfbd1edfd7
Ben Norskov <ben@playistheantidote.com>
2016-09-03 11:42:07 -0400

    I put in functionality to access a different GameObject than the one the script is on. I also added a surprise behind the door!

---
+++ Week01_Thursday/Assets/openDoor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class openDoor : MonoBehaviour {
+
+	void OnTriggerEnter() {
+		//Access another gameObject called "door" and set it to inactive
+		GameObject.Find("door").SetActive(false);
+		//play the door open sound. 
+		GetComponent<AudioSource>().Play();
+	}
+}
