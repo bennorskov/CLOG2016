8a9e0cafd143377f01859b3f97e96cf604279006
Ben Norskov <ben@playistheantidote.com>
2016-09-13 17:20:33 -0400

    In Class Example

---
+++ Week03 Tuesday/Assets/killOtherObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class killOtherObject : MonoBehaviour {
+	int lives = 30;
+
+	void OnTriggerExit( Collider otherObj ) {
+		otherObj.transform.position = otherObj.transform.position + Vector3.up * 40;
+		otherObj.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+		Debug.Log("thing moving out of the box");
+	}
+}
