ccfc420836f0e7cdfc4937cfe990f6c13cd9ccbc
Ben Norskov <ben@playistheantidote.com>
2016-10-09 17:08:15 -0400

    Rays and Recasting! File structure for Tuesday's class files.

---
+++ Week06/Assets/drawRay.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class drawRay : MonoBehaviour {
+
+	void Update() {
+		RaycastHit hitNFO;
+
+		if (Physics.Raycast(transform.position, transform.forward, out hitNFO, 10)) {
+			print("distance is: " + hitNFO.distance);
+			hitNFO.transform.GetComponent<Renderer>().material.color = Color.green;
+		}
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.green;
+		Gizmos.DrawRay(transform.position, transform.forward * 10);
+	}
+}
---
+++ Week06/Assets/moveRayDrawer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveRayDrawer : MonoBehaviour {
+	public void turnLeft() {
+		transform.Rotate(Vector3.up * 2);
+	}
+	public void turnRight() {
+		transform.Rotate(Vector3.up * -2);
+	}
+}
