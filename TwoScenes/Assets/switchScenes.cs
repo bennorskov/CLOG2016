2ad42c72d0aaa19e1f2a31adce16b6b77795e196
Ben Norskov <ben@playistheantidote.com>
2016-12-08 18:15:25 -0500

    In Class Example of Passing a Game Object between scenes

---
+++ TwoScenes/Assets/switchScenes.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class switchScenes : MonoBehaviour {
+
+	public static switchScenes thisScript;
+	// the "static" means there can only be one instance of this variable
+
+	void Awake () {
+		if (thisScript == null) {
+			DontDestroyOnLoad(gameObject);
+			thisScript = this;
+			// store a reference to this instance of the switchScenes class
+		} else if (thisScript != this) {
+			// if "thisScript" is not the current instance of the switchScenes class
+			Destroy(gameObject); //destroy this gameObject
+		}
+	}
+	
+	void Update () {
+		if (Input.GetKey(KeyCode.D)) {
+			SceneManager.LoadScene("Scene2");
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			SceneManager.LoadScene("Scene1");
+		}
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
